8b2c674a82c2f1fdedad0ef337c76afa8dfef4aa
HIMRAJ <himrajdas7@gmail.com>
2022-10-31 02:31:50 +0530

    checking odd or even problem done

---
+++ src/com/Himraj/OddEven.java
@@ -0,0 +1,17 @@
+package com.Himraj;
+
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        }
+        else {
+            System.out.println("Odd");
+        }
+    }
+}
