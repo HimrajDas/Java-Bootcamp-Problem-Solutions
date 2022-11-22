8a12858cf02bf333d877d406fa7f08f303ce1a70
HIMRAJ <himrajdas7@gmail.com>
2022-11-22 20:13:32 +0530

    palindrome problem solved

--- src/com/Himraj/Palindrome.java
+++ src/com/Himraj/Palindrome.java
@@ -1,5 +1,26 @@
 package com.Himraj;
 
+import java.util.Scanner;
 public class Palindrome {
-        
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number you want to check: ");
+        int number = sc.nextInt();
+        int ans = checkPalindrome(number);
+        if (ans == number) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    static int checkPalindrome(int number) {
+        int revNum = 0;
+        while (number > 0) {
+            int reminder = number % 10;
+            number /= 10;
+            revNum = revNum * 10 + reminder;
+        }
+        return revNum;
+    }
 }
